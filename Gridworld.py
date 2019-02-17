d8a429fcb0504cce2290e8bce9be7fb1c4000183
yugantjoshi <joshi.yugant@gmail.com>
2019-02-16 20:53:14 -0500

    build gridworld functions

--- Gridworld.py
+++ Gridworld.py
@@ -1,168 +1,94 @@
 import random
 import pygame
-import Cell as cell
-pygame.init()
+import numpy as np
+from Cell import Cell
 
-WHITE = (255,255,255)
-GREY = (20,20,20)
-BLACK = (0,0,0)
-PURPLE = (100,0,100)
-RED = (255,0,0)
 
-size = (701,701)
-screen = pygame.display.set_mode(size)
-done = False
+def create_maze():
 
-clock = pygame.time.Clock()
+    #Colors
+    WHITE = (255, 255, 255)
+    GREY = (20, 20, 20)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
 
-width = 25
-cols = int(size[0] / width)
-rows = int(size[1] / width)
+    # Screen Settings
+    screen_size = (700,700)
+    WIDTH = 70
+    screen = pygame.display.set_mode(screen_size)
 
-stack = []
+    done = False
 
-class Cell():
-    def __init__(self,x,y):
-        global width
-        self.x = x * width
-        self.y = y * width
+    # Num cols and rows based on screen size and width
+    num_rows = int(screen_size[1] / WIDTH)
+    num_cols = int(screen_size[0] / WIDTH)
 
-        self.visited = False
-        self.current = False
+    cell_stack = []
 
-        self.walls = [True,True,True,True] # top , right , bottom , left
+    def draw(cell):
+        if cell.get_current == True:
+            choice = np.random.choice(['unblocked', 'blocked'],
+                             1,
+                             p=[0.3, 0.7])
+            print(choice)
 
-        # neighbors
-        self.neighbors = []
 
-        self.top = 0
-        self.right = 0
-        self.bottom = 0
-        self.left = 0
 
-        self.next_cell = 0
+    def check_neighbors(cell):
+        pass
 
-    def draw(self):
-        if self.current:
-            pygame.draw.rect(screen,RED,(self.x,self.y,width,width))
-        elif self.visited:
-            pygame.draw.rect(screen,WHITE,(self.x,self.y,width,width))
+    def remove_walls(current_cell, next_cell):
+        pass
 
-            if self.walls[0]:
-                pygame.draw.line(screen,BLACK,(self.x,self.y),((self.x + width),self.y),1) # top
-            if self.walls[1]:
-                pygame.draw.line(screen,BLACK,((self.x + width),self.y),((self.x + width),(self.y + width)),1) # right
-            if self.walls[2]:
-                pygame.draw.line(screen,BLACK,((self.x + width),(self.y + width)),(self.x,(self.y + width)),1) # bottom
-            if self.walls[3]:
-                pygame.draw.line(screen,BLACK,(self.x,(self.y + width)),(self.x,self.y),1) # left
+    #Create gridworld with cells
+    gridworld = []
+    for y in range(num_rows):
+        gridworld.append([])
+        for x in range(num_cols):
+            cell = Cell(x,y, WIDTH)
+            gridworld[y].append(cell)
+            print(cell.get_coordinate())
 
-    def checkNeighbors(self):
-        #print("Top; y: " + str(int(self.y / width)) + ", y - 1: " + str(int(self.y / width) - 1))
-        if int(self.y / width) - 1 >= 0:
-            self.top = grid[int(self.y / width) - 1][int(self.x / width)]
-        #print("Right; x: " + str(int(self.x / width)) + ", x + 1: " + str(int(self.x / width) + 1))
-        if int(self.x / width) + 1 <= cols - 1:
-            self.right = grid[int(self.y / width)][int(self.x / width) + 1]
-        #print("Bottom; y: " + str(int(self.y / width)) + ", y + 1: " + str(int(self.y / width) + 1))
-        if int(self.y / width) + 1 <= rows - 1:
-            self.bottom = grid[int(self.y / width) + 1][int(self.x / width)]
-        #print("Left; x: " + str(int(self.x / width)) + ", x - 1: " + str(int(self.x / width) - 1))
-        if int(self.x / width) - 1 >= 0:
-            self.left = grid[int(self.y / width)][int(self.x / width) - 1]
-        #print("--------------------")
+    current_cell = gridworld[0][0]
+    next_cell = 0
 
-        if self.top != 0:
-            if self.top.visited == False:
-                self.neighbors.append(self.top)
-        if self.right != 0:
-            if self.right.visited == False:
-                self.neighbors.append(self.right)
-        if self.bottom != 0:
-            if self.bottom.visited == False:
-                self.neighbors.append(self.bottom)
-        if self.left != 0:
-            if self.left.visited == False:
-                self.neighbors.append(self.left)
+    while not done:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                done = True
 
-        if len(self.neighbors) > 0:
-            self.next_cell = self.neighbors[random.randrange(0,len(self.neighbors))]
-            return self.next_cell
-        else:
-            return False
+        screen.fill(GREY)
 
-def removeWalls(current_cell,next_cell):
-    x = int(current_cell.x / width) - int(next_cell.x / width)
-    y = int(current_cell.y / width) - int(next_cell.y / width)
-    if x == -1: # right of current
-        current_cell.walls[1] = False
-        next_cell.walls[3] = False
-    elif x == 1: # left of current
-        current_cell.walls[3] = False
-        next_cell.walls[1] = False
-    elif y == -1: # bottom of current
-        current_cell.walls[2] = False
-        next_cell.walls[0] = False
-    elif y == 1: # top of current
-        current_cell.walls[0] = False
-        next_cell.walls[2] = False
+        # Mark visited and current
+        print(current_cell)
+        current_cell.set_visited(True)
+        current_cell.set_current(True)
 
-grid = []
+        # Draw for current cell
+        for y in range(num_rows):
+            for x in range(num_cols):
+                draw(gridworld[y][x])
 
-for y in range(rows):
-    grid.append([])
-    for x in range(cols):
-        grid[y].append(Cell(x,y))
+        # Get next cell to go to
+        next_cell = check_neighbors(current_cell)
 
-current_cell = grid[0][0]
-next_cell = 0
+        # If next cell is valid
+        if next_cell != False:
+            # Clear neighbors list for new cell
+            current_cell.set_neighbors([])
+            # add current cell to stack
+            cell_stack.append(current_cell)
+            # Remove walls
+            remove_walls(current_cell, next_cell)
+            # Go to next cell
+            current_cell.set_current(False)
+            current_cell = next_cell
 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        # Pop new cell from remaining cells in stack
+        elif len(cell_stack) > 0:
+            current_cell.set_current(False)
+            current_cell = cell_stack.pop()
 
-    screen.fill(GREY)
+        pygame.display.flip()
 
-    current_cell.visited = True
-    current_cell.current = True
-
-    for y in range(rows):
-        for x in range(cols):
-            grid[y][x].draw()
-
-    next_cell = current_cell.checkNeighbors()
-
-    if next_cell != False:
-        current_cell.neighbors = []
-
-        stack.append(current_cell)
-
-        removeWalls(current_cell,next_cell)
-
-        current_cell.current = False
-
-        current_cell = next_cell
-
-    elif len(stack) > 0:
-        current_cell.current = False
-        current_cell = stack.pop()
-
-    elif len(stack) == 0:
-        grid = []
-
-        for y in range(rows):
-            grid.append([])
-            for x in range(cols):
-                grid[y].append(Cell(x,y))
-
-        current_cell = grid[0][0]
-        next_cell = 0
-
-    pygame.display.flip()
-
-    clock.tick(60)
-
-pygame.quit()
+create_maze()
