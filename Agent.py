8c8a497bca9bc24e52231eb4f83802362254e97e
yugantjoshi <joshi.yugant@gmail.com>
2019-02-20 15:04:13 -0500

    update Agent class

--- Agent.py
+++ Agent.py
@@ -2,19 +2,29 @@ import heapq
 
 
 class Agent:
-    UP = 0
-    RIGHT = 1
-    DOWN = 2
-    LEFT = 3
 
     def __init__(self, row, column):
+        self.row = row
+        self.column = column
         self.coordinate = row, column
 
     def set_coordinate(self, agent_coordinate):
         self.coordinate = agent_coordinate
 
+    def set_row(self, row):
+        self.row = row
+
+    def set_column(self, column):
+        self.column = column
+
     def get_coordinate(self):
         return self.coordinate
+
+    def get_row(self):
+        return self.row
+
+    def get_column(self):
+        return self.column
 
 
 
