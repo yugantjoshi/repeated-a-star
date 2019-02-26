7888e18b26abaaaf22f44815fb2eeb9cf8671823
yugantjoshi <joshi.yugant@gmail.com>
2019-02-26 03:17:37 -0500

    code cleanup, fix error with tuple

--- Cell.py
+++ Cell.py
@@ -1,173 +1,51 @@
 
-class Cell():
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    def __init__(self, row, col, width):
-
-        self.row = row * width
-        self.col = col * width
-
-        self.x = col
-        self.y = row
-
+        self.blocked = False
         self.visited = False
-        self.current = False
 
-        self.is_blocked = False
+        self.g = 0
+        self.h = 0
+        self.f = 0
 
-        self.g_value = -1
-        self.h_value = -1
-        self.f_value = -1
-
-        self.walls = [True, True, True, True]  # top , right , bottom , left
-
-        # neighbors
         self.neighbors = []
-
-        self.top = 0
-        self.right = 0
-        self.bottom = 0
-        self.left = 0
-
         self.parent = ()
-
-    def set_top(self, top):
-        self.top = top
-
-    def set_bottom(self, bottom):
-        self.bottom = bottom
-
-    def set_right(self, right):
-        self.right = right
-
-    def set_left(self, left):
-        self.left = left
 
     def set_neighbors(self, neighbors):
         self.neighbors = neighbors
         for n in neighbors:
-            n.set_g_value = self.g_value+1
-            self.update_f_value()
+            n.g = n.g+1
 
+    def set_g_value(self, a_x, a_y):
+        self.g = abs(a_x - self.x) + abs(a_y - self.y)
+        self.f = self.g + self.h
 
-    def set_x(self, x):
-        self.x = x
-    def set_y(self, y):
-        self.y = y
-    def get_x(self):
-        return self.x
-    def get_y(self):
-        return self.y
-    def add_neighbor(self, s):
-        self.neighbors.append(s)
-
-    def set_visited(self, isVisited):
-        self.visited = isVisited
-
-    def set_current(self, isCurrent):
-        self.current = isCurrent
-
-    def set_is_blocked(self, is_blocked):
-        self.is_blocked = is_blocked
-
-    def set_parent(self, cell):
-        self.parent = cell
-
-    def set_g_value(self, agent_coordinate):
-        # Pull Row and Column for agent from tuple
-        agent_row = agent_coordinate[0]
-        agent_column = agent_coordinate[1]
-
-        # Calculate Manhattan Distance for current cell
-        manhattan_distance = abs(agent_row - self.y) + abs(agent_column - self.x)
-
-        self.g_value = manhattan_distance
-        self.f_value = self.g_value + self.h_value
-
-    def set_h_value(self, target_coordinate):
-        # Pull Row and Column for target from tuple
-        target_row = target_coordinate[0]
-        target_column = target_coordinate[1]
-
-        # Calculate Manhattan Distance for current cell
-        manhattan_distance = abs(target_row - self.x) + abs(target_column - self.y)
-
-        self.h_value = manhattan_distance
-
-    def update_f_value(self):
-        self.f_value = self.g_value + self.h_value
+    def set_h_value(self, t_x, t_y):
+        self.h = abs(t_x - self.x) + abs(t_y - self.y)
 
     def __lt__(self, other):
-        return self.get_f_value() < other.get_f_value()
+        return self.f < other.f
 
     def get_coordinate(self):
         return self.x, self.y
 
-    def get_visited(self):
-        return self.visited
-
-    def get_current(self):
-        return self.current
-
-    def get_row(self):
-        return self.row
-
-    def get_column(self):
-        return self.col
-
-    def get_is_blocked(self):
-        return self.is_blocked
-
-    def get_g_value(self):
-        # Return Manhattan Distance
-        return self.g_value
-
-    def get_h_value(self):
-        return self.h_value
-
-    def get_f_value(self):
-        return self.f_value
-
-    def get_neighbors(self):
-        return self.neighbors
-
-    def get_neighbor(self, i):
-        return self.neighbors[i]
-
-    def get_parent(self):
-        return self.parent
-
-    def get_top(self):
-        return self.top
-
-    def get_bottom(self):
-        return self.bottom
-
-    def get_right(self):
-        return self.right
-
-    def get_left(self):
-        return self.left
-
     def get_best_neighbor(self, tie):
-        # Return neighbor with the smallest f value
-        minNeighbor = self.neighbors[0]
+        minN = self.neighbors[0]
+
         for n in self.neighbors:
-            if not n.get_visited():
-                if n.get_f_value() < minNeighbor.get_f_value():
-                    if n.get_f_value() == minNeighbor.get_f_value():
+            if not n.visited:
+                if n.f < minN.f:
+                    if n.f == minN.f:
                         # Tie Break
-                        if tie:  # use gval
-                            if n.get_g_value() < minNeighbor.get_g_value():
-                                minNeighbor = n
-                            #pass
-                        else:  # use hval
-                            if n.get_h_value() < minNeighbor.get_h_value():
-                                minNeighbor = n
-                            #pass
+                        if tie:  # Use g
+                            if n.g < minN.g:
+                                minN = n
+                        else:  # Use h
+                            if n.h < minN.h:
+                                minN = n
                     else:
-                        minNeighbor = n
-        return minNeighbor
-
-
-
-
+                        minN = n
+        return minN
--- Gridworld.py
+++ Gridworld.py
@@ -1,297 +1,145 @@
-import random
 import pygame
+import random as rand
 import numpy as np
 import heapq as heap
 from Cell import Cell
 
-
-#Colors
+# Colors
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 BLACK = (0, 0, 0)
-RED = (178,34,34)
-GREEN = (34,139,34)
+RED = (255,0,00)
+GREEN = (0,250,154)
 BLUE = (0,191,255)
 
-# Screen Settings
-screen_size = (700, 700)
-WIDTH = 7
+screen_size = (707, 707)
+width = 7
 screen = pygame.display.set_mode(screen_size)
 
-
-def forwardAStar(agent_initial_coord, target_coord, gridworld, tie):
-    # True tie means prefer gval when choosing tied neighbors
-    # False tie means prefer hval when choosing tied neighbors
-
+def forwardAStar(agent_initial_cell, target_cell, gridworld, tie):
+    # True tie means prefer gval, False means prefer hval
     openlist = []
     closedlist = []
-    start_cell = gridworld[agent_initial_coord[0]][agent_initial_coord[1]]
-    start_cell.set_g_value(agent_initial_coord)
+    agent_initial_cell.g = 0
 
-    heap.heappush(openlist, (start_cell.get_f_value(), start_cell))
+    heap.heappush(openlist, (agent_initial_cell.f, agent_initial_cell))
 
     while len(openlist) > 0:
-
         current_cell_tuple = heap.heappop(openlist)
         current_cell = current_cell_tuple[1]
-        pygame.draw.rect(screen, BLUE, (current_cell.get_x(), current_cell.get_y(), WIDTH, WIDTH))
 
-        heap.heappush(closedlist, current_cell)
-        current_cell.set_visited(True)
-        current_coord = current_cell.get_coordinate()
-        current_cell.set_g_value(5,3)
+        pygame.draw.rect(screen, BLUE, (current_cell.x * width, current_cell.y * width, width, width))
 
-        if current_cell.get_x() == target_coord[0] and current_cell.get_y() == target_coord[1]:
-            return getShortestPath(agent_initial_coord, target_coord, gridworld), closedlist
+        heap.heappush(closedlist, current_cell_tuple)
+        current_cell.visited = True
+        current_cell.set_g_value(current_cell.x, current_cell.y)
 
-        # Look at neighbors neighbors
-        i = current_cell.get_x()
-        j = current_cell.get_y()
+        # Reached target, return shortest path list
+        if current_cell.x == target_cell.x and current_cell.y == target_cell.y:
+            return get_shortest_path(agent_initial_cell.x, agent_initial_cell.y, target_cell.x, target_cell.y, gridworld), closedlist
 
-        # Get all neighbors for current cell
+        i = current_cell.x
+        j = current_cell.y
+
+        # Calculate all neighbors
         neighbors = [gridworld[x[0]][x[1]] for x in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)] if
                      x[0] >= 0 and x[1] >= 0 and x[0] < len(gridworld) and x[1] < len(gridworld[0])]
-        # Add neighbors to open list
 
         # Set neighbors list for that cell
         current_cell.set_neighbors(neighbors)
 
         # Add all unblocked neighbors to open list
-        updated_neighbors = current_cell.get_neighbors()
+        updated_neighbors = current_cell.neighbors
         for n in updated_neighbors:
-            if not n.get_is_blocked():
-                heap.heappush(openlist, (n.get_f_value(), n))
+            if not n.blocked:
+                heap.heappush(openlist, (n.f, n))
 
-        # Take neighbor with smallest f value and add to open list
         next_cell = current_cell.get_best_neighbor(tie)
-        next_cell.set_parent(current_cell.get_coordinate)
+        next_cell.parent = (current_cell.x, current_cell.y)
 
 
-def getShortestPath(agent_initial_coord, target_coord, gridworld):
-    ptr= gridworld[target_coord[0]][target_coord[1]]
-    path = [gridworld[target_coord[0]][target_coord[1]]]
-    while True:
-        ptr = ptr.get_parent()
-        path.append(ptr)
-        if ptr == gridworld[agent_initial_coord[0]][agent_initial_coord[1]]:
-            break
-        # ptr.state='path'
-    return path
+
+def get_shortest_path(a_x, a_y, t_x, t_y, gridworld):
+    pass
+
+def draw_cell(cell):
+    if cell.blocked:
+        pygame.draw.rect(screen, BLACK, (cell.x * width, cell.y * width, width, width))
+    else:
+        pygame.draw.rect(screen, WHITE, (cell.x * width, cell.y * width, width, width))
 
 
-def pick_next_cell(curr_cell, target_cell, width):
-
-    row = 0
-    col = 0
-
-    flag = True
-
-    if curr_cell == target_cell:
-        return curr_cell
-
-    if curr_cell[0] < target_cell[0]:
-        row = curr_cell[0] + width
-        col = curr_cell[1]
-        flag = False
-    elif curr_cell[0] > target_cell[0]:
-        row = curr_cell[0] - width
-        col = curr_cell[1]
-        flag = False
-    elif curr_cell[0] == target_cell[0]:
-        row = curr_cell[0]
-
-    if curr_cell[1] < target_cell[1] and flag:
-        col = curr_cell[1] + width
-        row = curr_cell[0]
-    elif curr_cell[1] > target_cell[1] and flag:
-        col = curr_cell[1] - width
-        row = curr_cell[0]
-    elif curr_cell[1] == target_cell[1]:
-        col = curr_cell[1]
-
-    next_cell = (row, col)
-
-    return next_cell
-
-def create_maze(agent, target):
-
-
+def create_maze():
     done = False
     clock = pygame.time.Clock()
 
-    # Num cols and rows based on screen size and width
-    num_rows = int(screen_size[1] / WIDTH)
-    num_cols = int(screen_size[0] / WIDTH)
+    # Num rows and cols
+    num_rows = int(screen_size[0] / width)
+    num_cols = int(screen_size[1] / width)
 
-    cell_stack = []
-
-    def draw2(cell):
-        if cell.get_is_blocked():
-            pygame.draw.rect(screen, BLACK, (cell.get_row(), cell.get_column(), WIDTH, WIDTH))
-        else:
-            pygame.draw.rect(screen, WHITE, (cell.get_row(), cell.get_column(), WIDTH, WIDTH))
-
-
-    def check_neighbors(cell):
-
-        # Top
-        if int(cell.get_column() / WIDTH) - 1 >= 0:
-            gridworld_x = (cell.get_column() / WIDTH) - 1
-            gridworld_y = (cell.get_row() / WIDTH)
-            cell.set_top(gridworld[int(gridworld_x)][int(gridworld_y)])
-        # Right
-        if int(cell.get_row() / WIDTH) + 1 <= num_cols - 1:
-            gridworld_x = (cell.get_column() / WIDTH)
-            gridworld_y = (cell.get_row() / WIDTH) + 1
-            cell.set_right(gridworld[int(gridworld_x)][int(gridworld_y)])
-        # Bottom
-        if int(cell.get_column() / WIDTH) + 1 <= num_rows - 1:
-            gridworld_x = (cell.get_column() / WIDTH) + 1
-            gridworld_y = (cell.get_row() / WIDTH)
-            cell.set_bottom(gridworld[int(gridworld_x)][int(gridworld_y)])
-        # Left
-        if int(cell.get_row() / WIDTH) - 1 >= 0:
-            gridworld_x = (cell.get_column() / WIDTH)
-            gridworld_y = (cell.get_row() / WIDTH) - 1
-            cell.set_left(gridworld[int(gridworld_x)][int(gridworld_y)])
-
-        # Top
-        if cell.get_top() != 0:
-            if cell.get_top().get_visited() == False:
-                cell.add_neighbor(cell.get_top())
-                cell.get_top().get_visited()
-        # Right
-        if cell.get_right() != 0:
-            if cell.get_right().get_visited() == False:
-                cell.add_neighbor(cell.get_right())
-        # Bottom
-        if cell.get_bottom() != 0:
-            if cell.get_bottom().get_visited() == False:
-                cell.add_neighbor(cell.get_bottom())
-        # Left
-        if cell.get_left() != 0:
-            if cell.get_left().get_visited() == False:
-                cell.add_neighbor(cell.get_left())
-
-        if len(cell.get_neighbors()) > 0:
-            cell.set_next_cell(cell.get_neighbor(random.randrange(0,len(cell.get_neighbors()))))
-            return cell.get_next_cell()
-        else:
-            return False
-
-    #Create gridworld with cells
+    global stack
+    stack = []
+    global gridworld
     gridworld = []
+
+    #  Create grid of cells
     for y in range(num_rows):
         gridworld.append([])
         for x in range(num_cols):
-            cell = Cell(x,y, WIDTH)
+            cell = Cell(x, y)
             gridworld[y].append(cell)
 
+    global current_cell
     current_cell = gridworld[0][0]
 
-    random_list = [1, 2, 3, 4]
-
+    # Mark cells blocked or unblocked with probability
     for x in range(num_rows):
         for y in range(num_cols):
-
-            num_chosen1 = random.choice(random_list)
-
-            if num_chosen1 == 1:
-
-                num_chosen = random.choice(random_list)
-
-                #top
-                if num_chosen == 1:
-                    if x != 0:
-                        gridworld[x - 1][y].set_is_blocked(True)
-                #bottom
-                elif num_chosen == 2:
-                    if x < num_rows - 1:
-                        gridworld[x + 1][y].set_is_blocked(True)
-                #right
-                elif num_chosen == 3:
-                    if y < num_cols - 1:
-                        gridworld[x][y + 1].set_is_blocked(True)
-                #left
-                elif num_chosen == 4:
-                    if y != 0:
-                        gridworld[x][y - 1].set_is_blocked(True)
-
-
-    next_cell = 0
+            choice = np.random.choice(['blocked', 'unblocked'], 1, p=[0.3, 0.7])
+            if choice == 'blocked':
+                gridworld[x][y].blocked = True
 
     count = 0
 
-    '@Summary: This loop generates the maze randomly and also sets the agent and target'
     while count != 1:
-
         if count == 0:
-
             screen.fill(GRAY)
 
-            # Mark visited and current
-            current_cell.set_visited(True)
-            current_cell.set_current(True)
+            # Mark current cell as visited
+            current_cell.visited = True
 
-            # Draw for current cell
+            # Draw current cell
             for y in range(num_rows):
                 for x in range(num_cols):
-                    draw2(gridworld[y][x])
+                    draw_cell(gridworld[y][x])
 
-        if len(cell_stack) == 0:
-
+        if len(stack) == 0:
             if count == 0:
+                global agent_initial_cell
+                agent_initial_cell = rand.choice(rand.choice(gridworld))
+                pygame.draw.rect(screen, GREEN, (agent_initial_cell.x*width, agent_initial_cell.y*width, width, width))
 
-                agentCell = random.choice(random.choice(gridworld))
-                pygame.draw.rect(screen, GREEN, (agentCell.get_row(), agentCell.get_column(), WIDTH, WIDTH))
-                agent = (agentCell.get_row(), agentCell.get_column())
-                print(agent)
-
-                targetCell = random.choice(random.choice(gridworld))
-                pygame.draw.rect(screen, RED, (targetCell.get_row(), targetCell.get_column(), WIDTH, WIDTH))
-                target = targetCell.get_row(), targetCell.get_column()
-                print(target)
+                global target_cell
+                target_cell = rand.choice(rand.choice(gridworld))
+                pygame.draw.rect(screen, RED, (target_cell.x*width, target_cell.y*width, width, width))
 
                 for x in range(num_rows):
                     for y in range(num_cols):
-                        agent_coord = agent[0]/WIDTH, agent[1]/WIDTH
-                        target_coord = target[0]/WIDTH, target[0]/WIDTH
-                        gridworld[x][y].set_h_value(target_coord)
-
-                count = 1
+                        gridworld[x][y].set_h_value(target_cell.x, target_cell.y)
+            count = 1
 
         pygame.display.flip()
 
-    curr_cell = agent
-    end_cell = target
+    current_cell = agent_initial_cell
 
-
-
-    '@Summary: This is the loop that is constantly running behind the scenes and updates the display'
     while not done:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 done = True
 
-        agent_coord = int(curr_cell[0] / WIDTH), int(curr_cell[1] / WIDTH)
-        target_coord = int(end_cell[0] / WIDTH), int(end_cell[1] / WIDTH)
-
-        forwardAStar(agent_coord, target_coord, gridworld, True)
-
-        '''As the agent Moves along the path update within this loop and color path that is taken'''
-
-        ''' color_next_cell = pick_next_cell(curr_cell, end_cell, WIDTH)
-
-        if color_next_cell != target:
-            pygame.draw.rect(screen, BLUE, (color_next_cell[0], color_next_cell[1], WIDTH, WIDTH))
-
-        curr_cell = color_next_cell '''
-
-
+        forwardAStar(agent_initial_cell, target_cell, gridworld, True)
         pygame.display.flip()
-        pygame.time.delay(100)
-        #clock.tick(6000)
+        pygame.time.delay(10)
+        clock.tick(30)
 
-agent = ()
-target = ()
-create_maze(agent, target)
+create_maze()
